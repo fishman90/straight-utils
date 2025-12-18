1ad695236a130e75bd533fca01dd89f10da60977
yuchunzhou <enjoylife.czzz@gmail.com>
2025-12-18 18:56:00 +0800

    refactor

--- straight-utils.el
+++ straight-utils.el
@@ -1,17 +1,17 @@
 ;;-*- lexical-binding: t; -*-
 
-(defvar straight-utils-install-buffer-name " *Install straight-utils* ")
+(defvar straight-utils-compilation-buffer-name "*straight-utils*")
 
-(defun straight-utils-module--cargo-is-available ()
+(defun straight-utils-module-check-cargo-is-available ()
   (unless (executable-find "cargo")
-    (error "Straight-Utils needs Rust to be compiled.  Please, install Rust"))
+    (error "straight-utils needs Rust to be compiled.  Please install Rust!"))
   t)
 
 ;;;###autoload
 (defun straight-utils-module-compile ()
   "Compile straight-utils-module."
   (interactive)
-  (when (straight-utils-module--cargo-is-available)
+  (when (straight-utils-module-check-cargo-is-available)
     (let* ((straight-utils-directory
             (shell-quote-argument
              (file-name-directory
@@ -20,21 +20,21 @@
             (format
 	     "cd %s; cargo build --release; cp target/release/libstraight_utils_module.so straight-utils-module.so; cd -" straight-utils-directory))
            (buffer
-	    (get-buffer-create straight-utils-install-buffer-name)))
+	    (get-buffer-create straight-utils-compilation-buffer-name)))
       (pop-to-buffer buffer)
       (compilation-mode)
       (if (zerop
 	   (let ((inhibit-read-only t))
              (call-process "sh" nil buffer t "-c" build-commands)))
-          (message "Compilation of `straight-utils-module' succeeded")
+          (message "Compilation of `straight-utils-module' module succeeded!")
         (error "Compilation of `straight-utils-module' module failed!")))))
 
 (unless (require 'straight-utils-module nil t)
-  (if (y-or-n-p "Straight-Utils needs `straight-utils-module' to work.  Compile it now? ")
+  (if (y-or-n-p "straight-utils needs `straight-utils-module' to work. Compile it now?")
       (progn
 	(straight-utils-module-compile)
 	(require 'straight-utils-module))
-    (error "Straight-Utils will not work until `straight-utils-module' is compiled!")))
+    (error "straight-utils will not work until `straight-utils-module' module is compiled!")))
 
 ;;;###autoload
 (defun straight-utils-pull-all ()
